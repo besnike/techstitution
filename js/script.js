fa55fac01df55c5bb1b4403de5363243213ce5ca
besnike <muratibesnike@gmail.com>
2017-11-23 10:42:26 +0100

    changes made

---
+++ js/script.js
@@ -0,0 +1,10 @@
+console.log(5+6);
+console.log("hello world");
+function consoleTeDhenat(emri,mbiemri,vendlindja)
+{
+	console.log(emri,mbiemri,vendlindja);
+	
+}
+consoleTeDhenat("Besnike","Murati","Prishtine");
+
+window.alert("You visited Techstitution");

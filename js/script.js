5b6a82d33203d8f612cff58a9b063456abcc75f7
besnike <muratibesnike@gmail.com>
2017-11-23 14:37:43 +0100

    New files

--- js/script.js
+++ js/script.js
@@ -1,10 +1,2 @@
-console.log(5+6);
-console.log("hello world");
-function consoleTeDhenat(emri,mbiemri,vendlindja)
-{
-	console.log(emri,mbiemri,vendlindja);
-	
-}
-consoleTeDhenat("Besnike","Murati","Prishtine");
 
-window.alert("You visited Techstitution");
+

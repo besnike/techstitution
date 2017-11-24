501893987cc94ed839e7d0fd8e374426bd401b58
besnike <muratibesnike@gmail.com>
2017-11-24 13:36:16 +0100

    changed form

--- js/script.js
+++ js/script.js
@@ -1,2 +1,24 @@
-
+$(document).ready (function(){
+	console.log("DOM is ready!")
+	
+	 $('#border-wait-form').submit(function(event){
+		 event.preventDefault()
+		 console.log ("Form!")
+		 // hyrje daljet ne minuta
+		 var pikaKufitare = $('#pika-kufitare').val();
+		 var hyrjeMinMinuta = $('#hyrje-min-minuta').val();
+		 var hyrjeMaxMinuta = $ ('#hyrje-max-minuta').val();
+		 var daljeMinMinuta = $('#dalje-min-minuta').val();
+		 var daljeMaxMinuta = $('#dalje-max-minuta').val();
+		 // hyrje daljet ne metra
+		  var hyrjeMinMetra = $('#hyrje-min-metra').val();
+		 var hyrjeMaxMetra = $ ('#hyrje-max-metra').val();
+		 var daljeMinMetra = $('#dalje-min-metra').val();
+		 var daljeMaxMetra = $('#dalje-max-metra').val();
+	 
+		 console.log ( "Pika Kufitare:", pikaKufitare, "Hyrje min:", hyrjeMinMinuta, "Hyrje max:", hyrjeMaxMinuta, 
+		             "Dalje min:", daljeMinMinuta, "Dalje max:", daljeMaxMinuta, "Hyrje min:", hyrjeMinMetra, 
+					 "Hyrje max:", hyrjeMaxMetra, "Dalje min:", daljeMinMetra, "Dalje max:", daljeMaxMetra)
+	 });
+});
 
